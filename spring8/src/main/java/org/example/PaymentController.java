31d27e33d1f089c8dacb9223dbe1c4a4db3fd897
AngryZpUa <zpcitizen@gmail.com>
2023-02-16 18:02:38 +0200

    Add commit with RestTemplate

---
+++ spring8/src/main/java/org/example/PaymentController.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+    private final PaymentProxy paymentProxy;
+
+    public PaymentController(PaymentProxy paymentProxy) {
+        this.paymentProxy = paymentProxy;
+    }
+
+    @PostMapping("/payment")
+    public Payment createPayment(@RequestBody Payment payment) {
+        return paymentProxy.createPayment(payment);
+    }
+}
